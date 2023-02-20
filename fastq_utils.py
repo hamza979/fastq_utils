9e92509861b4b5dbff15dcfafe7837afd615318f
hamza979 <hamzahasan362@gmail.com>
2023-02-20 16:02:23 +0000

    Implemented UAT 2 and corresponding test case

--- fastq_utils.py
+++ fastq_utils.py
@@ -1,7 +1,7 @@
 import argparse
 
 def count_sequences(filename):
-    """Count the number of non-blank lines in a FASTQ file"""
+    """Count the number of non-blank lines in a FASTQ file and divide by 4"""
     with open(filename, "r") as file:
         count = 0
         for line in file:
@@ -9,18 +9,30 @@ def count_sequences(filename):
                 count += 1
     count=count // 4
     print(f"Number of sequences: {count}")
+    
+    
+def count_nucleotides(filename):
+    """Sum the len of each field 2 line"""
+    count = 0
+    with open(filename, "r") as file:
+        for i, line in enumerate(file):
+            if i % 4 == 1:
+                count += len(line.strip())
+    print(f"Number of nucleotides: {count}")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Utility functions for FASTQ files")
     parser.add_argument("filename", help="FASTQ file to process")
-    parser.add_argument("-n", "--count-sequences", action="store_true", help="Count the number of sequences in the file")
+    parser.add_argument("--count-sequences", action="store_true", help="Count the number of sequences in the file")
+    parser.add_argument("--count-nucleotides", action="store_true", help="Count the total number of nucleotides in the file")
     args = parser.parse_args()
 
     if args.count_sequences:
         count_sequences(args.filename)
-    else:
-        # Perform other operations here
-        pass
+    elif args.count_nucleotides:
+        count_nucleotides(args.filename)
+
 
 if __name__ == "__main__":
     main()
--- test_fastq_utils.py
+++ test_fastq_utils.py
@@ -4,3 +4,7 @@ def test_sequence_count(capsys):
 	fastq_utils.count_sequences("test1.fastq")
 	captured=capsys.readouterr()
 	assert captured.out.strip() == "Number of sequences: 1000"
+def test_nucleotide_count(capsys):
+	fastq_utils.count_nucleotides("test1.fastq")
+	captured=capsys.readouterr()
+	assert captured.out.strip() == "Number of nucleotides: 100000"
